3ce60520e4d0529cd04ccb7f034bd2486b029ad6
Dayun417 <leedayun03417@gmail.com>
2024-12-30 01:02:44 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241230/369 게임/369-game.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    scanf("%d", &n);
+
+    for(i=1;i<=n;i++) {
+        if(i%3==0)  {
+            printf("0 ");
+        } 
+        else if(i>10) {
+            if((i%10)%3==0 || (i/10)%3==0) {
+                printf("0 ");
+            }
+        }
+        else {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}

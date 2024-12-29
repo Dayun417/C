72fd542613cfca40c18cf4adc27fda964bfdce8e
Dayun417 <leedayun03417@gmail.com>
2024-12-29 22:43:03 +0900

    Title: 1까지 나누기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241229/1까지 나누기/divide-by-1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, cnt = 0;
+    scanf("%d", &n);
+
+    for (i = 1; i++;) {
+        if (n / i > 1) {
+            n /= i;
+            cnt++;
+        }
+        if (n / i <= 1) {
+            break;
+        }
+        cnt++;
+    }
+
+    printf("%d", cnt);
+
+    return 0;
+}

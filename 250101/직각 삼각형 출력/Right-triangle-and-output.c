0a4c93a749f9768cd3f8c2c5ed28dfb118efd54a
Dayun417 <leedayun03417@gmail.com>
2025-01-01 22:23:34 +0900

    Title: 직각 삼각형 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250101/직각 삼각형 출력/Right-triangle-and-output.c
+++ 250101/직각 삼각형 출력/Right-triangle-and-output.c
@@ -2,19 +2,13 @@
 
 int main() {
     int n;
-    scanf("%d", &n);
+	scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) {
-            if (j == 0) {
-                printf("*");
-            }
-            else {
-                printf("**");
-            }
-        }
-        printf("\n");
-    }
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 2 * i + 1; j++)
+			printf("*");
+		printf("\n");
+	}
 
     return 0;
 }

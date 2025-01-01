a93ddf6fdfa6381260d97688091ce47722eaf3e8
Dayun417 <leedayun03417@gmail.com>
2025-01-01 16:57:08 +0900

    Title: ab 사이에 없는 c, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250101/ab 사이에 없는 c/c-between-a-and-b-2.c
+++ 250101/ab 사이에 없는 c/c-between-a-and-b-2.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdboool.h>
+
 int main() {
     int a, b, c;
     bool satified = true;
 
     scanf("%d %d %d", &a, &b, &c);
-    
+
     for(int i=a;i<=b;i++) {
-        if(i%c==0) {
+        if(i%c!=0) {
             satified = false;
         }
     }

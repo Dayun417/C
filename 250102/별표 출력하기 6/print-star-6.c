fc7394181ddfb721b9ce0285ff103b4480d412d4
Dayun417 <leedayun03417@gmail.com>
2025-01-02 00:52:27 +0900

    Title: 별표 출력하기 6, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250102/별표 출력하기 6/print-star-6.c
+++ 250102/별표 출력하기 6/print-star-6.c
@@ -2,9 +2,9 @@
 
 int main() {
     int n;
-    scanf("%d", &n);
+	scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < i; j++) {
 			printf("  ");
 		}
@@ -13,7 +13,7 @@ int main() {
 		}
 		printf("\n");
 	}
-    for (int i = 0; i <=2; i++) {
+	for (int i = 0; i < n-1; i++) {
 		for (int j = 0; j < n - i - 2; j++) {
 			printf("  ");
 		}

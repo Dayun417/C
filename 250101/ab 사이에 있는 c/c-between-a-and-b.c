beeb1b76f3fac71cd3bcf641dae139056c396d47
Dayun417 <leedayun03417@gmail.com>
2025-01-01 16:37:21 +0900

    Title: ab 사이에 있는 c, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250101/ab 사이에 있는 c/c-between-a-and-b.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int a, b, c;
+    bool satisfied = false;
+    scanf("%d %d %d", &a, &b, &c);
+
+    for(int i=a;i<=b;i++) {
+        if(i%7==0) {
+            satisfied = true;
+        }
+        if(satisfied==true) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+
+    return 0;
+}

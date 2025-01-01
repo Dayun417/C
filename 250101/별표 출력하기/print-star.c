1f80f8b44a774c5f3eda0e672d944ec6b804691d
Dayun417 <leedayun03417@gmail.com>
2025-01-01 23:21:04 +0900

    Title: 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250101/별표 출력하기/print-star.c
+++ 250101/별표 출력하기/print-star.c
@@ -6,14 +6,14 @@ int main() {
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j <= i; j++) {
-			printf("*");
+			printf("* ");
 		}
 		printf("\n");
 	}
 
 	for (int i = n-2; i >= 0; i--) {
 		for (int j = 0; j <= i; j++) {
-			printf("*");
+			printf("* ");
 		}
 		printf("\n");
 	}

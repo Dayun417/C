6c9aae589fa223a42f03f90b7cc52ed864005c7e
Dayun417 <leedayun03417@gmail.com>
2025-01-05 21:47:18 +0900

    Title: 별표 출력하기 13, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250105/별표 출력하기 13/print-start-13.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    scanf("%d", &n);
+
+    for(i=0;i<n;i++) {
+        if(i%2!=0) {
+            for(j=0;j<1+i/2;j++) {
+                printf("* ");
+            }
+        } else {
+            for(j=0;j<n-i/2;j++) {
+                printf("* ");
+            }
+        }
+        printf("\n");       
+    }
+
+    for(i=n;i>0;i--) {
+        if(i%2!=0) {
+            for(j=0;j<n-i/2;j++) {
+                printf("* ");
+            }
+        } else {
+            for(j=0;j<i/2;j++) {
+                printf("* ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

a82256cb661ba00469e3b4141b20d6492738b763
Dayun417 <leedayun03417@gmail.com>
2025-01-06 22:08:14 +0900

    Title: 모양대로 수 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250106/모양대로 수 출력하기/print-out-numbers-in-specific-shape.c
+++ 250106/모양대로 수 출력하기/print-out-numbers-in-specific-shape.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);
+  int n;
+  scanf("%d", &n);
 
-    for(int i = 0; i < n; i++) {
-		for(int j = 0; j < 2*i; j++)
-			printf(" ");
-        for(int j = n; j > i; j--)
-			printf("%d ", j-i);  
-		printf("\n");
-    }
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < 2*i; j++)
+      printf(" ");
+    for(int j = n; j > i; j--)
+      printf("%d ", j-i);  
+    printf("\n");
+  }
 
-    return 0;
+  return 0;
 }

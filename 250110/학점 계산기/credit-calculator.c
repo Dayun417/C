682c0adef29999cd74b519a005cc21c9e2194cce
Dayun417 <leedayun03417@gmail.com>
2025-01-10 12:06:48 +0900

    Title: 학점 계산기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250110/학점 계산기/credit-calculator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, cnt=0;
+    scanf("%d", &n);
+    double arr[100], sum=0, avg;
+
+    for(int i=0;i<n;i++) {
+        scanf("%lf ", &arr[i]);
+        sum+=arr[i];
+        cnt++;
+    }
+    avg = sum/cnt;
+    printf("%.1lf", avg);
+
+    if(avg>=4.0) {
+        printf("Perfect");
+    } else if(avg>=3.0) {
+        printf("Good");
+    } else {
+        printf("Poor");
+    }
+    
+    return 0;
+}

375a47a3aea5f33bae949d60ec61371a9525a532
Dayun417 <leedayun03417@gmail.com>
2025-01-11 15:13:24 +0900

    Title: 특정 위치의 문자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250111/특정 위치의 문자 2/char-in-specific-location-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char arr[10];
+
+    for(int i=0;i<10;i++) {
+        scanf("%c ", &arr[i]);
+    }
+
+    for(int i=1;i<10;i+=3) {
+        printf("%c ", arr[i]);
+    }
+    
+
+    return 0;
+}

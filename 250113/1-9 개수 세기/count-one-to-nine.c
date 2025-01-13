f84fd56802143d72d3fa4251dd8d4067e38875b3
Dayun417 <leedayun03417@gmail.com>
2025-01-13 20:48:44 +0900

    Title: 1-9 개수 세기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 250113/1-9 개수 세기/count-one-to-nine.c
+++ 250113/1-9 개수 세기/count-one-to-nine.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    scanf("%d", &n); // 입력 개수 받기
-
-    int arr[10];
-    int cnt_arr[10] = {0}; // 개수 저장 배열
-
-    // 배열 입력받기
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    // 입력된 숫자의 개수 세기
-    for (int i = 0; i < n; i++) {
-        if (arr[i] >= 1 && arr[i] <= 9) { // 1~9 사이인지 확인
-            cnt_arr[arr[i]]++;
-        }
-    }
-
-    // 1부터 9까지의 개수 출력
-    for (int i = 1; i < 10; i++) {
-        printf("%d\n", cnt_arr[i]);
+	int n;
+	scanf("%d", &n);
+	int arr[100];
+	int count_arr[10] = {0,};
+	
+	// arr[i]를 입력받은 후 카운팅 배열을 통해 각각의 빈도 저장
+	for(int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+		count_arr[arr[i]]++;
+	}
+	
+    // 1부터 9까지 나온 횟수를 출력
+    for(int i = 1; i <= 9; i++) {
+        printf("%d\n", count_arr[i]);
     }
 
     return 0;

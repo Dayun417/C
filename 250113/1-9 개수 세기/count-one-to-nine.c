26d40249a99b2df7b5ed3d198dab158aa15cb2b1
Dayun417 <leedayun03417@gmail.com>
2025-01-13 20:46:29 +0900

    Title: 1-9 개수 세기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 250113/1-9 개수 세기/count-one-to-nine.c
+++ 250113/1-9 개수 세기/count-one-to-nine.c
@@ -1,33 +1,28 @@
-
 #include <stdio.h>
 
 int main() {
-	int n;
-	scanf("%d", &n);
-	int arr[10];
-	int cnt_arr[10] = { 0, };
+    int n;
+    scanf("%d", &n); // 입력 개수 받기
 
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &arr[i]);
+    int arr[10];
+    int cnt_arr[10] = {0}; // 개수 저장 배열
 
-		// 입력값 검증
-        if (arr[i] < 0 || arr[i] > 9) {
-            printf("Invalid input: %d\n", arr[i]);
-            return 1; // 프로그램 종료
+    // 배열 입력받기
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // 입력된 숫자의 개수 세기
+    for (int i = 0; i < n; i++) {
+        if (arr[i] >= 1 && arr[i] <= 9) { // 1~9 사이인지 확인
+            cnt_arr[arr[i]]++;
         }
-	}
+    }
 
-	// 개수 세기
-	for (int i = 0; i < n; i++) {
-		cnt_arr[arr[i]]++;
-	}
+    // 1부터 9까지의 개수 출력
+    for (int i = 1; i < 10; i++) {
+        printf("%d\n", cnt_arr[i]);
+    }
 
-	// 개수 출력
-	for (int i = 1; i < 10; i++) {
-		printf("%d\n", cnt_arr[i]);
-	}
-
-	return 0;
-
+    return 0;
 }
-

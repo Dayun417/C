55d052d5cf0acd3903732e269358c42e40515628
Dayun417 <leedayun03417@gmail.com>
2025-01-13 16:16:49 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250113/특정 조건에 맞게 출력하기/print-in-specific-conditions.c
+++ 250113/특정 조건에 맞게 출력하기/print-in-specific-conditions.c
@@ -3,21 +3,23 @@
 int main() {
     int arr[100];
 
-    for(int i=0;i<11;i++) {
+    for(int i=0;i<100;i++) {
         scanf("%d ", &arr[i]);
         if(arr[i]==0) {
             break;
         }
     }
 
-    for(int i=0;i<10;i++) {
+    for(int i=0;i<100;i++) {
         if(arr[i]%2==0) {
-            arr[i]=arr[i]/2;
-            printf("%d ", arr[i]);
+            arr[i]/=2;
         } else {
-            arr[i]=arr[i]+3;
-            printf("%d ", arr[i]);
+            arr[i]+=3;
         }
+        if(arr[i]==0) {
+            break;
+        }
+        printf("%d ", arr[i]);
     }
 
     return 0;

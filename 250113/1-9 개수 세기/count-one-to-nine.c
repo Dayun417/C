22193267f46726bd0f0e1db4b779f6563ac4659a
Dayun417 <leedayun03417@gmail.com>
2025-01-13 20:38:44 +0900

    Title: 1-9 개수 세기, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 250113/1-9 개수 세기/count-one-to-nine.c
+++ 250113/1-9 개수 세기/count-one-to-nine.c
@@ -5,20 +5,26 @@ int main() {
 	int n;
 	scanf("%d", &n);
 	int arr[10];
-	int count_arr[10] = { 0, };
+	int cnt_arr[10] = { 0, };
 
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &arr[i]);
+
+		// 입력값 검증
+        if (arr[i] < 0 || arr[i] > 9) {
+            printf("Invalid input: %d\n", arr[i]);
+            return 1; // 프로그램 종료
+        }
 	}
 
 	// 개수 세기
 	for (int i = 0; i < n; i++) {
-		count_arr[arr[i]]++;
+		cnt_arr[arr[i]]++;
 	}
 
 	// 개수 출력
 	for (int i = 1; i < 10; i++) {
-		printf("%d\n", count_arr[i]);
+		printf("%d\n", cnt_arr[i]);
 	}
 
 	return 0;

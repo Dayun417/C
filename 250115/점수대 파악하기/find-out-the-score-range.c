2db6cf3b2fd72bf41727ff4d0871edeaa19d2592
Dayun417 <leedayun03417@gmail.com>
2025-01-15 22:10:05 +0900

    Title: 점수대 파악하기, Time: 17ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250115/점수대 파악하기/find-out-the-score-range.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], cnt[11]={0,};
+
+    for(int i=0;i<=100;i+=10) {
+        scanf("%d ", &arr[i]);
+
+        if(arr[i]==0) {
+            break;
+        }
+        if(arr[i]<10) {
+            continue;
+        }
+        cnt[arr[i]/10]++;
+    }
+
+    for(int i=10;i>0;i--) {
+        printf("%d - %d\n", i*10, cnt[i]);
+    }
+
+    return 0;
+}

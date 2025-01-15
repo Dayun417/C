b8a40bbf0f20469cd64490083c5e6c0637465b95
Dayun417 <leedayun03417@gmail.com>
2025-01-16 01:37:47 +0900

    Title: 정수 직사각형, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250116/정수 직사각형/rectangle-of-numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int arr[3][5]={0,};
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int num=1;
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
+            arr[i][j]=num;
+            num++;
+        }
+    }
+
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

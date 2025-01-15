fa1744c7eb2c0dafc33f708c315d45ef8a6d9eee
Dayun417 <leedayun03417@gmail.com>
2025-01-16 01:57:36 +0900

    Title: 두 개의 격자 비교하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250116/두 개의 격자 비교하기/compare-two-grid.c
+++ 250116/두 개의 격자 비교하기/compare-two-grid.c
@@ -5,18 +5,18 @@ int main() {
     scanf("%d %d", &n, &m);
 
     for(int i=0;i<n;i++) {
-        for(int j=0;j<n;j++) {
+        for(int j=0;j<m;j++) {
             scanf("%d", &arr[i][j]);
         }
     }
     for(int i=0;i<n;i++) {
-        for(int j=0;j<n;j++) {
+        for(int j=0;j<m;j++) {
             scanf("%d", &arr2[i][j]);
         }
     }
 
     for(int i=0;i<n;i++) {
-        for(int j=0;j<n;j++) {
+        for(int j=0;j<m;j++) {
             if(arr[i][j]==arr2[i][j]) {
                 printf("0 ");
             } else {

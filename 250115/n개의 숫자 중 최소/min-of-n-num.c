f2b07312825b2feeef15da54c8d342fe17e7091f
Dayun417 <leedayun03417@gmail.com>
2025-01-15 15:56:13 +0900

    Title: n개의 숫자 중 최소, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250115/n개의 숫자 중 최소/min-of-n-num.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int n, arr[100], cnt=0;
+    int min = INT_MAX;
+    scanf("%d", &n);
+    
+    for(int i=0;i<n;i++) {
+        scanf("%d ", &arr[i]);
+    }
+    
+    for(int i=0;i<n;i++) {
+        if(min>arr[i]) {
+            min=arr[i];
+            cnt++;
+        }
+    }
+    printf("%d %d", min, cnt);
+
+    return 0;
+}
